bdc373e81e868d89ee814657cf686961cce7237d
Ricardobrito1 <ricardo.brito@bandtec.com.br>
2021-11-29 17:54:23 -0300

    ultima versão docker

--- AssistenteDeInstalação.sh
+++ AssistenteDeInstalação.sh
@@ -7,24 +7,19 @@ echo " "
 echo "======================================================="
 echo " "
 
-echo " Bem-vindo ao assistente de instalação da Edutec"
+echo " Bem-vindo ao assistente de instalação do Class Sight"
 echo " "
 
-echo "Iremos instalar alguns itens necessarios para a inicialização da nossa aplicação"
-echo " "
+echo "Durante o processo de instalação iremos atualizar seus pacotes e tambem baixar alguns software(Java e Maven) nescessarios para nossa aplicação rodar perfeitamente"
 
-echo "Em alguns momentos iremos solicitar que você responda com 'Y ou N' ou caso esteja em portugues 'S ou N'"
-echo " "
-
-echo "Antes de começar iremos atualizar os pacotes do seu pc"
 echo " "
 echo " "
 echo "Podemos dar inicio a instalação?"
 echo "1 - SIM"
 echo "2 - Não"
-read comecar
+read resposta
 
-if [ "$comecar" -eq "1" ];
+if [ "$resposta" -eq "1" ];
 then
 
 sudo apt-get update
@@ -38,45 +33,34 @@ fi
 
 echo " "
 echo " "
-
-echo "=========================================================================================================="
-echo " "
-echo "Realizando o download do  java (Se ja estiver instalado ele irá mostrar a versão)"
-echo " "
-echo "=========================================================================================================="
 java -version || sudo apt-get install default-jdk -y
 
 echo " "
 echo " "
 
-echo "=========================================================================================================================="
-echo " "
-echo "Instalando o maven (Se ja estiver instalado ele irá mostrar a versão)"
-echo " "
-echo "==========================================================================================================================="
 mvn -version || sudo apt install maven
 
 echo " "
 echo " "
-echo "Está quase tudo pronto agora iremos instalar nossa aplicação"
+echo "Agora iremos baixar a aplicação"
 echo " "
-echo "No meio da instalação será criada uma pasta para onde terá nossos arquivos"
+echo "Iremos criar um diretorio para armazenar nossa aplicação"
 
 git clone https://github.com/Ricardobrito1/jar-edutec.git
 
 cd  jar-edutec
 
 echo " "
-echo "$estamos nesse caminho do seu computador "
+echo "diretorio criado com a aplicação >>>>"
 pwd
 
 echo " "
 echo "Deseja iniciar o programa?"
 echo "1 - SIM "
 echo "2 - NÃO"
-read ini
+read start
 
-if [ "$ini" -eq "1" ];
+if [ "$start" -eq "1" ];
 then
 java -jar Edutec-Vm.jar
 else
